0f85bd559ca161279d9e75b0a9a0ae14fd849852
mo5tone <menjiefu@gmail.com>
2019-08-24 08:14:16 +0800

    init commit.

---
+++ iOmicTests/iOmicTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOmicTests.swift
+//  iOmicTests
+//
+//  Created by 门捷夫 on 2019/8/24.
+//  Copyright © 2019 门捷夫. All rights reserved.
+//
+
+import XCTest
+@testable import iOmic
+
+class iOmicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
