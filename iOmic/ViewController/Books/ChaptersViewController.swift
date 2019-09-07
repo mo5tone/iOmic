4c4f608afd335f4df08428f31c77d02e8119388d
mo5tone <menjiefu@gmail.com>
2019-09-07 10:37:44 +0800

    - Add book detail header

--- iOmic/ViewController/Books/ChaptersViewController.swift
+++ iOmic/ViewController/Books/ChaptersViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 门捷夫. All rights reserved.
 //
 
+import Hue
 import Kingfisher
 import MarqueeLabel
 import RxCocoa
@@ -22,10 +23,15 @@ class ChaptersViewController: UIViewController {
     private weak var coordinator: ChaptersViewCoordinator?
     private let viewModel: ChaptersViewModel
     private lazy var titleLabel: MarqueeLabel = .init()
-    private lazy var coverImageView: UIImageView = .init()
-    private let coverImageViewAspectRatio: CGFloat = 16 / 16
     private lazy var refreshControl: UIRefreshControl = .init()
     @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var headerContainerView: UIView!
+    @IBOutlet var coverImageView: UIImageView!
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var updateAtLabel: UILabel!
     private let dataSource: RxCollectionViewSectionedAnimatedDataSource<AnimatableSectionModel<Int, Chapter>> = .init(configureCell: { _, collectionView, indexPath, chapter in
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChapterCollectionViewCell.reusableIdentifier, for: indexPath)
         if let cell = cell as? ChapterCollectionViewCell { cell.titleLabel.text = chapter.name }
@@ -55,19 +61,12 @@ class ChaptersViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.prefersLargeTitles = false
-        navigationController?.navigationBar.isTranslucent = true
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.navigationBar.isTranslucent = false
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         // !!!: - the safeAreaInsets would be zero in viewDidLoad
-        collectionView.contentInset = .init(top: coverImageView.frame.height + 8 - view.safeAreaInsets.top, left: 8, bottom: 8, right: 8)
-        view.constraints.filter { $0.firstAttribute == .top && $0.firstItem === coverImageView }.first?.constant = 0 - view.safeAreaInsets.top
+        collectionView.contentInset = .init(top: headerContainerView.frame.maxY + 4 - view.safeAreaInsets.top, left: 8, bottom: 8, right: 8)
     }
 
     // MARK: - private instance methods
@@ -79,31 +78,70 @@ class ChaptersViewController: UIViewController {
         titleLabel.speed = .duration(4)
         titleLabel.animationCurve = .linear
         titleLabel.fadeLength = 4
-        titleLabel.leadingBuffer = 8
-        navigationItem.titleView = titleLabel
+        titleLabel.leadingBuffer = 4
+        titleLabel.trailingBuffer = 4
+        navigationItem.titleView = {
+            /// https://stackoverflow.com/questions/20094198
+            let view = UIView()
+            view.addSubview(titleLabel)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                titleLabel.widthAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.width / 2),
+            ])
+            return view
+        }()
 
-        view.addSubview(coverImageView)
-        coverImageView.isOpaque = false
-        coverImageView.contentMode = .scaleAspectFill
-        coverImageView.clipsToBounds = true
-        coverImageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            coverImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-            coverImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-            coverImageView.heightAnchor.constraint(equalTo: coverImageView.widthAnchor, multiplier: 1 / coverImageViewAspectRatio),
-            coverImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0 - view.safeAreaInsets.top),
-        ])
         collectionView.backgroundColor = .groupTableViewBackground
         collectionView.refreshControl = refreshControl
         collectionView.registerCell(ChapterCollectionViewCell.self)
+
+        headerContainerView.isOpaque = false
+        coverImageView.contentMode = .scaleAspectFill
+        coverImageView.layer.cornerRadius = 8.0
+        coverImageView.layer.masksToBounds = true
+        descriptionTextView.isEditable = false
+        descriptionTextView.showsVerticalScrollIndicator = false
+        descriptionTextView.showsHorizontalScrollIndicator = false
+        descriptionTextView.font = .preferredFont(forTextStyle: .body)
+        authorLabel.font = .preferredFont(forTextStyle: .caption1)
+        statusLabel.font = .preferredFont(forTextStyle: .caption2)
+        genreLabel.font = .preferredFont(forTextStyle: .caption2)
+        updateAtLabel.font = .preferredFont(forTextStyle: .caption2)
     }
 
     private func setupBinding() {
-        viewModel.book.compactMap { $0.thumbnailUrl }.subscribe(onNext: { [weak self] in self?.coverImageView.kf.setImage(with: URL(string: $0), options: [.processor(BlurImageProcessor(blurRadius: 20)), .scaleFactor(UIScreen.main.scale), .cacheOriginalImage]) }).disposed(by: bag)
+        viewModel.book.subscribe(onNext: { [weak self] book in
+            guard let self = self else { return }
+
+            self.coverImageView.kf.setImage(with: URL(string: book.thumbnailUrl ?? ""), options: [.transition(.fade(0.2)), .requestModifier(book.source.modifier), .scaleFactor(UIScreen.main.scale), .cacheOriginalImage]) { result in
+                var backgroundColor: UIColor
+                switch result {
+                case let .success(image):
+                    backgroundColor = image.image.colors().primary
+                case .failure:
+                    backgroundColor = .groupTableViewBackground
+                }
+                self.headerContainerView.backgroundColor = backgroundColor
+                self.descriptionTextView.backgroundColor = backgroundColor
+                let textColor: UIColor = backgroundColor.isDark ? .lightText : .darkText
+                self.descriptionTextView.textColor = textColor
+                self.authorLabel.textColor = textColor
+                self.statusLabel.textColor = textColor
+                self.genreLabel.textColor = textColor
+                self.updateAtLabel.textColor = textColor
+            }
+            self.descriptionTextView.text = book.description
+            self.authorLabel.text = book.author
+            self.statusLabel.text = book.status.name
+            self.genreLabel.text = book.genre
+        }).disposed(by: bag)
         refreshControl.rx.controlEvent(.valueChanged).bind(to: viewModel.load).disposed(by: bag)
         viewModel.chapters.subscribe(onNext: { [weak self] _ in self?.refreshControl.endRefreshing() }).disposed(by: bag)
         viewModel.book.map { $0.title }.bind(to: titleLabel.rx.text).disposed(by: bag)
         collectionView.rx.setDelegate(self).disposed(by: bag)
+        viewModel.chapters.compactMap { chapters in chapters.compactMap { $0.updateAt }.sorted(by: { $0 < $1 }).last?.convert2String(dateFormat: "yyyy-MM-dd") }.bind(to: updateAtLabel.rx.text).disposed(by: bag)
         viewModel.chapters.map { [AnimatableSectionModel<Int, Chapter>(model: 0, items: $0)] }.bind(to: collectionView.rx.items(dataSource: dataSource)).disposed(by: bag)
     }
 }
@@ -128,6 +166,6 @@ extension ChaptersViewController: UICollectionViewDelegateFlowLayout {
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let alpha = max(0, min(1, 0 - scrollView.contentOffset.y / collectionView.contentInset.top))
-        coverImageView.alpha = alpha
+        headerContainerView.alpha = alpha
     }
 }
